c2af12cbd2d4be2fed6f6d206914b50274db6d31
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-10-09 22:22:56 +0100

    Add table

--- src/LogWindow.js
+++ src/LogWindow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './LogWindow.css'
 
 class LogWindow extends Component {
 
@@ -13,34 +14,70 @@ class LogWindow extends Component {
 
   getData = () => {
     const URL = "https://log-project-8c7ef.firebaseio.com/logs.json?orderBy=%22timestamp%22&startAt=60";
-    
+
     fetch(URL, {
       method: "GET"
     })
-    .then(response => response.json())
-    .then(data => {
-      this.setState({
-        data,
-        lastUpdate: new Date()
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          data,
+          lastUpdate: new Date()
+        })
       })
-    })
   }
 
   componentDidMount = () => {
-    window.setInterval(this.getData, 1000)
+    window.setInterval(this.getData, 1000);
+  }
+
+  renderEntry = (entry) => {
+    var date = new Date(entry.timestamp);
+    return (
+      <tr className="entry">
+        <td className="message">
+          {entry.message}
+        </td>
+        <td className="tag">
+          <i>{entry.tag}</i>
+        </td>
+        <td className="level">
+          <small>{entry.level}</small>
+        </td>
+        <td className="date">
+          {/* <small>{date.toString()}</small> */}
+        </td>
+      </tr>
+
+    )
   }
 
   render() {
-    if (!this.state.data) return null;
+    if (!this.state.data)
+      return "Loading log...";
 
-    var messages = Object.keys(this.state.data).map(key => {
-      return this.state.data[key].message;
+    var entries = Object.keys(this.state.data).map(key => {
+      return this.state.data[key];
     })
 
+    entries = entries.sort((a, b) => a.timestamp - b.timestamp);
+
     return (
-    <div>
-      {messages.map(text => <p>{text}</p>)}
-      <p>Last update: {this.state.lastUpdate.toString()} </p>
+      <div className="log">
+        <table>
+          <thead>
+            <tr>
+              <th align="left">Message</th>
+              <th align="left">Tag</th>
+              <th align="left">Level</th>
+            </tr>
+          </thead>
+          <tbody>
+            {entries.map(this.renderEntry)}
+          </tbody>
+        </table>
+        <p>Last update: {this.state.lastUpdate.toString()} </p>
     </div>
     );
   }
